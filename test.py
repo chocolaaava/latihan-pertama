57fdbeb00e61dd558f5d1d2fe616683e7ddb82f7
msseptia <mayangseptia45@gmail.com>
2021-07-19 22:38:27 +0700

    push pertama saya

---
+++ test.py
@@ -0,0 +1,10 @@
+hariIni = "rabu"
+
+if hariIni == "senin":
+    print("kuliah")
+elif hariIni == "selasa":
+    print("kuliah")
+elif hariIni == "rabu":
+    print("libur")
+else:
+    print("tidak ada jadwal")
